e5791e6489ed207bb2f966df860314e669d06c14
Alex Martin <alextravc.342@gmail.com>
2023-08-13 15:10:06 -0500

    [ADD]: JwtAuthFilter

--- src/main/java/com/skeleton/security/auth/service/CustomUserDetailsService.java
+++ src/main/java/com/skeleton/security/auth/service/CustomUserDetailsService.java
@@ -3,20 +3,22 @@ package com.skeleton.security.auth.service;
 import com.skeleton.security.auth.jwt.UserDetailsImpl;
 import com.skeleton.security.users.entity.Usuario;
 import com.skeleton.security.users.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepository;
+    // @Autowired in auto by constructor thanks to @RequiredArgsConstructor
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        // Todo: use userService to find user
         Usuario user = userRepository.findOneByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException(
                         "User not found with email: ".concat(email)));
